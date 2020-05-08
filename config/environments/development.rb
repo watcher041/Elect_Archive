4952bd2c8f0fc49765601db90074e0db9408598c
watcher041 <63713236+watcher041@users.noreply.github.com>
2020-05-08 14:16:37 +0900

    edit_confirmable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.default_url_options = { :host => 'heroku.elect-archive.com' }
+  config.action_mailer.default_url_options = { :host => 'elect-archive.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { 
     :enable_starttls_auto => true,
