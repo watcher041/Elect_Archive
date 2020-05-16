e72435fd68aa7cc9ee6ab8749ad33dac3ff4488a
watcher041 <63713236+watcher041@users.noreply.github.com>
2020-05-16 11:45:31 +0900

    edit_search_total_count

--- app/controllers/posts/searches_controller.rb
+++ app/controllers/posts/searches_controller.rb
@@ -1,6 +1,8 @@
 class Posts::SearchesController < ApplicationController
   def index
     result = Post.search(params[:keyword])
+
+    # メソッドを使用して作成したデータは配列扱いになるため、kaminari独自のメソッドでpagenateさせる
     @posts = Kaminari.paginate_array(result).page(params[:page]).per(3)
   end
 end
