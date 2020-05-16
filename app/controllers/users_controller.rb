7ad7a967f63cfbe23c326bcfef6adbfae4557092
watcher041 <63713236+watcher041@users.noreply.github.com>
2020-05-16 11:08:23 +0900

    edit_pagenate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.page(params[:page]).per(5).order("created_at DESC")
+    @posts = @user.posts.page(params[:page]).per(3).order("created_at DESC")
   end
 end
