8c8cc9ef996464e906f117a3da8b5be6e8f4ed06
watcher041 <63713236+watcher041@users.noreply.github.com>
2020-05-19 15:24:49 +0900

    user_delete_and_post

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  has_many :posts
+  has_many :posts, dependent: :destroy
   
   mount_uploader :image, ImageUploader
 
