ce720990636a7ff8f317f7cbc9d66aa9890e1069
watcher041 <63713236+watcher041@users.noreply.github.com>
2020-05-26 07:11:56 +0900

    edit_carreerwave.rb No2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1'　# S3バケット作成時に指定したリージョン。左記は東京を指す
     }
     config.fog_directory  = 'elect-archive' # 作成したS3バケット名
